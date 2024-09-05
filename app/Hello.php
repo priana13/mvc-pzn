76087769d2e30df7a3b96135d00c1c261905a6c2
priana13 <herian.sap@gmail.com>
2024-09-06 06:11:44 +0700

    simple MVC

---
+++ app/Hello.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Hello {
+
+    public $nama;
+
+    public function _construct(){
+
+         echo "tes";
+    }
+
+   
+
+}
+
+$hello = new Hello();
+
+
+echo $hello->nama;
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$path = '/index';
+
+if( isset($_SERVER['PATH_INFO']) ){
+
+    $path = $_SERVER['PATH_INFO'];
+}
+
+require __DIR__ . '/../app/View' . $path . '.php';
